b6680dfe1dd2d62f4837064cf88d9dcc0e323782
calum boustead <calumboustead@calums-mbp.mynet>
2020-07-01 20:45:12 +0100

    Initial commit

---
+++ CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by calum boustead on 01/07/2020.
+//  Copyright © 2020 Boustead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
